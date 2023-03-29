ac31b3577e5c4f657b297b63eba6529874e3b406
José Alexandre Cordeiro Souza <alexandrecordeiro15@outlook.com>
2023-03-29 12:23:47 -0300

    cumpre o requisito 4 e consegue 100% de score no stryker

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,20 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import NotFound from '../pages/NotFound';
+
+describe('Testes relacionados ao NotFound.js', () => {
+  it('Teste se a página contém um heading h2 com o texto Page requested not found;', () => {
+    render(<NotFound />);
+    const verifyHeading = screen.getByRole('heading', { name: 'Page requested not found' });
+
+    expect(verifyHeading).toBeInTheDocument();
+  });
+
+  it('Teste se a página mostra a imagem https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif.', () => {
+    render(<NotFound />);
+    const verifyImage = screen.getByRole('img', { name: /pikachu crying because the page requested was not found/i });
+
+    expect(verifyImage).toBeInTheDocument();
+    expect(verifyImage.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
